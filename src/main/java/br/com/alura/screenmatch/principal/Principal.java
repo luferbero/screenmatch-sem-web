5e06190000ea498e5625f08bbe3cbc437f1e7cc8
luferbero <luferberodrigues@gmail.com>
2024-11-30 11:13:34 -0300

    Criando nova interação com usuário e trazendo estatísticas sobre a série

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,20 +1,26 @@
 package br.com.alura.screenmatch.principal;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 public class Principal {
 	private Scanner leitura = new Scanner(System.in);
-	
 	private ConsumoApi consumo = new ConsumoApi();
-	
 	private ConverteDados conversor = new ConverteDados();
 	
 	private final String ENDERECO = "https://www.omdbapi.com/?t=";
@@ -23,9 +29,7 @@ public class Principal {
 	public void exibeMenu() {
 		System.out.println("Digite o nome da série para busca");
 		var nomeSerie = leitura.nextLine();
-
 		var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-		
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
 		
@@ -39,13 +43,30 @@ public class Principal {
 		
 		temporadas.forEach(System.out::println);
 		
-		/*for(int i = 0; i < dados.totalTemporadas(); i++) {
-			List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-			for(int j = 0; j < episodiosTemporada.size(); j++) {
-				System.out.println(episodiosTemporada.get(j).titulo());
-			}
-		}*/
-		
 		temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+		
+		List<DadosEpisodio> dadosEpisodios = temporadas .stream()
+				.flatMap(t -> t.episodios().stream())
+				.collect(Collectors.toList());
+				
+		List<Episodio> episodios = temporadas .stream()
+				.flatMap(t -> t.episodios().stream()
+				.map(d -> new Episodio(t.numero(), d)))
+				.collect(Collectors.toList());
+		
+		episodios.forEach(System.out::println);
+		
+		Map<Integer, Double> avaliacaoPorTemporada = episodios.stream()
+				.filter(e -> e.getAvaliacao() > 0.0)
+				.collect(Collectors.groupingBy(Episodio::getTemporada,Collectors.averagingDouble(Episodio::getAvaliacao)));
+		System.out.println(avaliacaoPorTemporada);
+		
+		DoubleSummaryStatistics est = episodios.stream()
+				.filter(e -> e.getAvaliacao() > 0.0)
+				.collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+		System.out.println("Média: " + est.getAverage());
+		System.out.println("Melhor episódio: " + est.getMax());
+		System.out.println("Pior episódio: " + est.getMin());
+		System.out.println("Quantidade: " + est.getCount());
 	}
 }
